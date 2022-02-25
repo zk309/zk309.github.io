0f2468555bb03ac35da061af1e80c7a2d385a175
zhang-ke <90496467+zhnag-ke@users.noreply.github.com>
2022-02-25 16:04:44 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @version: April 2021 (version 1.56)
+ * @Author: ZhangKe
+ * @Date: 2022-02-18 08:14:57
+ * @LastEditors: ZhangKe
+ * @LastEditTime: 2022-02-18 08:17:45
+ * @FilePath: \LearnHtml+Css\11-品优购案例\shoping\js\index.js
+ */
+// console.log(1);
+
+// 
